1ef2f70fe21f9e878264db6837cd664f8d71a6c7
WKmedia <WKmedia@users.noreply.github.com>
2023-11-19 15:20:29 -0500

    modifiedd webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports ={
+    mode: 'development',
     entry: {
         index: './src/index.js',
         print: './src/print.js',
