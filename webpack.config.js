3982e7237c60dcab14b77b957a0414f4832c7390
WKmedia <WKmedia@users.noreply.github.com>
2023-11-19 16:49:07 -0500

    config setup for dev and prod env with webpack

--- webpack.config.js
+++ webpack.config.js
@@ -8,6 +8,9 @@ module.exports ={
         print: './src/print.js',
     },
     devtool: 'inline-source-map',
+    devServer: {
+        static: './dist',
+    },
     plugins: [
         new HtmlWebpackPlugin({
             title: 'Output Management',
@@ -17,6 +20,9 @@ module.exports ={
         filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist'),
         clean: true,
+    },
+    optimization: {
+        runtimeChunk: 'single',
     },
     module: {
         rules: [
