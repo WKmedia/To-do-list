20bab720179261ee406cff18651d3a90b7d76a21
WKmedia <WKmedia@users.noreply.github.com>
2023-11-19 20:10:06 -0500

    added header image and title and side menu

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import printMe from './print.js';
 
 function component() {
     const element = document.createElement('div');
+
     const btn = document.createElement('button');
     const topBar = document.createElement('div');
       const topBarImage = document.createElement('div');
@@ -14,15 +15,15 @@ function component() {
         const navContainer = document.createElement('nav');
           const homeMenu = document.createElement('ul');
             const homeMenuItem1 = document.createElement('li');
-            const menuItemSpan1 = document.createElement('span');
+            const homeMenuItemSpan1 = document.createElement('span');
             const homeMenuItem2 = document.createElement('li');
-            const menuItemSpan2 = document.createElement('span');
+            const homeMenuItemSpan2 = document.createElement('span');
             const homeMenuItem3 = document.createElement('li');
-            const menuItemSpan3 = document.createElement('span');
+            const homeMenuItemSpan3 = document.createElement('span');
             const homeMenuItem4 = document.createElement('li');
-            const menuItemSpan4 = document.createElement('span');
+            const homeMenuItemSpan4 = document.createElement('span');
             const homeMenuItem5 = document.createElement('li');
-            const menuItemSpan5 = document.createElement('span');
+            const homeMenuItemSpan5 = document.createElement('span');
         
         const projectMenu = document.createElement('ul');
           const projectMenuItem1 = document.createElement('li');
@@ -40,7 +41,62 @@ function component() {
 
 
 
-element.appendChild(topBar)
+
+ 
+
+    element.classList.add('hello');
+    topBar.setAttribute('id', 'top-bar');
+    topBarImage.setAttribute('id', 'logo');
+    h1Header.setAttribute('id', 'header');
+    h1Header.textContent = 'To Do List';
+    sideBarContainer.setAttribute('id', 'side-bar-container');
+    sideBarNav.setAttribute('id', 'side-bar-nav');
+    navContainer.setAttribute('id', 'nav-container');
+    homeMenu.setAttribute('id', 'home-menu');
+
+    homeMenuItem1.setAttribute('class', 'home-menu-item');
+    homeMenuItem1.setAttribute('id', 'home-menu-item1');
+    homeMenuItemSpan1.setAttribute('id', 'home-menu-item1');
+    homeMenuItem1.textContent = 'Home';
+    homeMenuItemSpan1.textContent = 6;
+
+    homeMenuItem2.setAttribute('class', 'home-menu-item');
+    homeMenuItem2.setAttribute('id', 'home-menu-item2');
+    homeMenuItemSpan2.setAttribute('id', 'home-menu-item2');
+    homeMenuItem2.textContent = 'Today';
+    homeMenuItemSpan2.textContent = 5;
+    
+    homeMenuItem3.setAttribute('class', 'home-menu-item');
+    homeMenuItem3.setAttribute('id', 'home-menu-item3');
+    homeMenuItemSpan3.setAttribute('id', 'home-menu-item3');
+    homeMenuItem3.textContent = 'Upcoming';
+    homeMenuItemSpan3.textContent = 4;
+    
+    homeMenuItem4.setAttribute('class', 'home-menu-item');
+    homeMenuItem4.setAttribute('id', 'home-menu-item4');
+    homeMenuItemSpan4.setAttribute('id', 'home-menu-item4');
+    homeMenuItem4.textContent = 'Personal';
+    homeMenuItemSpan4.textContent = 3;
+    
+    homeMenuItem5.setAttribute('class', 'home-menu-item');
+    homeMenuItem5.setAttribute('id', 'home-menu-item5');
+    homeMenuItemSpan5.setAttribute('id', 'home-menu-item5');
+    homeMenuItem5.textContent = 'Work';
+    homeMenuItemSpan5.textContent = 2;
+
+    
+    
+    const myIcon = new Image();
+    myIcon.src = Icon;
+    btn.innerHTML = 'Click me and check the console!';
+    btn.onclick = printMe;
+
+   
+   
+      
+    // element.appendChild(myIcon);
+    element.append(topBar)
+    
   topBar.append(topBarImage)
   topBar.append(h1Header)
 element.appendChild(sideBarContainer)
@@ -48,15 +104,15 @@ element.appendChild(sideBarContainer)
    sideBarNav.append(navContainer)
     navContainer.append(homeMenu)
      homeMenu.append(homeMenuItem1)
-     homeMenuItem1.append(menuItemSpan1)
+     homeMenuItem1.append(homeMenuItemSpan1)
      homeMenu.append(homeMenuItem2)
-     homeMenuItem2.append(menuItemSpan2)
+     homeMenuItem2.append(homeMenuItemSpan2)
      homeMenu.append(homeMenuItem3)
-     homeMenuItem3.append(menuItemSpan3)
+     homeMenuItem3.append(homeMenuItemSpan3)
      homeMenu.append(homeMenuItem4)
-     homeMenuItem4.append(menuItemSpan4)
+     homeMenuItem4.append(homeMenuItemSpan4)
      homeMenu.append(homeMenuItem5)
-     homeMenuItem5.append(menuItemSpan5)
+     homeMenuItem5.append(homeMenuItemSpan5)
    
      navContainer.append(projectMenu)
      projectMenu.append(projectMenuItem1)
@@ -72,21 +128,8 @@ element.appendChild(sideBarContainer)
      noteLink.append(noteLinkItem)
 
      navContainer.append(addNewButton)
- 
 
-    element.classList.add('hello');
-    element.innerHTML = 'Hello';
-    const myIcon = new Image();
-    myIcon.src = Icon;
-    btn.innerHTML = 'Click me and check the console!';
-    btn.onclick = printMe;
-
-   
-   
-    element.appendChild(btn);    
-    element.appendChild(myIcon);
-
-
+     element.appendChild(btn); 
     return element;
 
 }
--- webpack.common.js
+++ webpack.common.js
@@ -5,6 +5,7 @@ module.exports = {
     mode: 'development',
     entry: {
         app: './src/index.js',
+        print: './src/print.js',
     },
     plugins: [
         new HtmlWebpackPlugin({
@@ -15,5 +16,24 @@ module.exports = {
         filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist'),
         clean: true,
+    },
+    optimization: {
+        runtimeChunk: 'single',
+      },
+      module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: 'asset/resource',
+            },
+        ],
     },
 };
